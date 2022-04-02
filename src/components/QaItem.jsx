a1310f9d1438e4b60394a18e1cab2d4f836f0f1e
peykani <peykani@gmail.com>
2022-04-02 23:49:18 +0300

    alternative solution

--- src/components/QaItem.jsx
+++ src/components/QaItem.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 
 
-const QaItem = ({qa,handleShow}) => {
+const QaItem = ({qa,onToggle}) => {
     const [showAnswer, setShowAnswer] = useState(false)
-console.log(handleShow)
+    
   return (
     <div>
-<div key={qa.id} className="qa-card">
-                <h5>{qa.qn} <i className={`fa-solid fa-circle-${showAnswer?"minus":"plus"}`} onClick={()=>handleShow(setShowAnswer,showAnswer,qa.id)}></i></h5>
-                {showAnswer&&<p key={qa.id}>{qa.ans}</p>
+<div className="qa-card">
+                <h5>{qa.qn} <i className={`fa-solid fa-circle-${showAnswer?"minus":"plus"}`} onClick={()=>setShowAnswer(!showAnswer)}></i></h5>
+                {showAnswer&&<p>{qa.ans}</p>
     }
             </div>
     </div>
